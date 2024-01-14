66c09cb69182437d362019b2b508d3e47ea175f0
“Amitsharma45” <sharma.amit20111@gmail.com>
2024-01-14 20:58:01 +0530

    fixed bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
-
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import ThemeProvider from "@/context/ThemeProvider";
 
